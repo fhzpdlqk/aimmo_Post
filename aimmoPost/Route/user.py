187d6dcc34ea98ceec3337f12a67e689673ecf43
fhzpdlqk <rozencrants@naver.com>
2022-06-28 17:53:31 +0900

    signup base feature

--- aimmoPost/Route/user.py
+++ aimmoPost/Route/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
 from ..models import User
 
 user = Blueprint("user", __name__, url_prefix="/user")
@@ -6,11 +6,14 @@ user = Blueprint("user", __name__, url_prefix="/user")
 
 @user.route("/login", methods=["POST", "GET"])
 def login():
-    post = User(user_id="admin", user_pw="admin")
-    post.save()
     return "login"
 
 
 @user.route("/", methods=["POST"])
 def signup():
-    return "signup"
+    content_type = request.headers.get("Content-Type")
+    if content_type == "application/json":
+        data = request.json
+        post = User(_id=data["user_id"], pw=data["user_pw"])
+        post.save()
+    return jsonify({"success": True})
--- aimmoPost/models.py
+++ aimmoPost/models.py
@@ -3,5 +3,5 @@ from . import config
 
 
 class User(Document):
-    user_id = StringField(required="True", max_length=200)
-    user_pw = StringField(required="True", max_length=100)
+    _id = StringField(required="True", max_length=200, primary_key=True)
+    pw = StringField(required="True", max_length=100)
