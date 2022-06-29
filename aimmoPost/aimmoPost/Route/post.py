4ffd7b0b2d6e0956942f9e98a27b7293c4620afd
fhzpdlqk <rozencrants@naver.com>
2022-06-29 18:21:43 +0900

    post register feature

---
+++ aimmoPost/aimmoPost/Route/post.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, jsonify, request
+from aimmoPost.aimmoPost.models import User, Post, Like, Comment
+from aimmoPost.aimmoPost.config import default
+import mongoengine
+import sys
+import jwt
+
+post = Blueprint("post", __name__, url_prefix="/post")
+token_key = default.token
+
+
+@post.route("/regist", methods=["POST"])
+def login():
+    try:
+        decoded = jwt.decode(request.headers["Token"], token_key, algorithms="HS256")
+        data = request.json
+        user_id = decoded["user_id"]
+        title = data["title"]
+        content = data["content"]
+        tag = data["tag"]
+        notice = data["notice"]
+        post = Post.Post(writer=user_id, title=title, content=content, tag=tag, notice=notice)
+        post.save()
+        return jsonify({"success": True})
+    except:
+        return jsonify({"success": False, "message": str(sys.exc_info()[0])})
+    return "post"
--- aimmoPost/aimmoPost/models/Post.py
+++ aimmoPost/aimmoPost/models/Post.py
@@ -6,11 +6,11 @@ import datetime
 
 
 class Post(Document):
-    writer = ReferenceField(User, required=True)
+    writer = StringField(required=True)
     date = DateTimeField(default=datetime.datetime.utcnow)
     title = StringField(required=True, max_length=100)
     content = StringField(required=True)
     tag = ListField(StringField(), default=list)
     notice = BooleanField(default=False)
-    comment = ReferenceField(Comment)
-    like = ReferenceField(Like)
+    comment = ListField(ReferenceField(Comment), default=list)
+    like = ListField(ReferenceField(Like), default=list)
