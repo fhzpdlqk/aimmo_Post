1d630a4ad543105f7adba67d3036e3a54aa8373b
fhzpdlqk <rozencrants@naver.com>
2022-06-29 15:17:30 +0900

    signup pw encryption

--- aimmoPost/aimmoPost/app.py
+++ aimmoPost/aimmoPost/app.py
@@ -2,14 +2,16 @@ __version__ = "0.1.0"
 import os
 from flask import Flask, jsonify
 
-
 from .config import default
-import aimmoPost.aimmoPost.Route as Route
+from aimmoPost.aimmoPost.Route import user
 from mongoengine import connect
 from flask_cors import CORS
 
 connect("connect1", host="mongodb://" + default.mongodb_host + ":" + default.mongodb_port + "/" + default.mongodb_name)
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "abcd"
+app.config["BCRYPT_LEVEL"] = 10
+
 CORS(app)
-app.register_blueprint(Route.user.user)
+app.register_blueprint(user.user)
