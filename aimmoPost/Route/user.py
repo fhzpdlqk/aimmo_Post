3427f6e72b07a14a4adc488405fa8c9ca5aa44d5
fhzpdlqk <rozencrants@naver.com>
2022-06-29 10:10:56 +0900

    id duplicate message & marshmellow setting

--- aimmoPost/Route/user.py
+++ aimmoPost/Route/user.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify, request
 from ..models import User
+import mongoengine
+import sys
 
 user = Blueprint("user", __name__, url_prefix="/user")
 
@@ -9,11 +11,31 @@ def login():
     return "login"
 
 
+"""
+    회원가입 API
+    method : POST
+    content-type: application/json
+    request : {
+        user_id : 윺저 아이디 (String),
+        user_pw : 유저 비밀번호 (String)
+    }
+    response : {
+        status : 200, success: true
+        status : 300, success: true
+    }
+"""
+
+
 @user.route("/", methods=["POST"])
 def signup():
     content_type = request.headers.get("Content-Type")
     if content_type == "application/json":
-        data = request.json
-        post = User(_id=data["user_id"], pw=data["user_pw"])
-        post.save()
-    return jsonify({"success": True})
+        try:
+            data = request.json
+            User.User(user_id=data["user_id"], user_pw=data["user_pw"]).save()
+            return jsonify({"success": True})
+        except mongoengine.errors.NotUniqueError:
+            return jsonify({"success": False, "message": "id가 중복되었습니다."})
+        except:
+            print(sys.exc_info()[0])
+            return jsonify({"success": False})
---
+++ aimmoPost/models/User.py
@@ -0,0 +1,6 @@
+from mongoengine import *
+
+
+class User(Document):
+    user_id = StringField(required="True", max_length=200, unique=True)
+    user_pw = StringField(required="True", max_length=100)
