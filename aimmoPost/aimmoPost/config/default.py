e679c44ff8c0c0dfc524c84ea7c387e9526fe95c
fhzpdlqk <rozencrants@naver.com>
2022-06-29 16:24:43 +0900

    response add logintoken

--- aimmoPost/aimmoPost/config/default.py
+++ aimmoPost/aimmoPost/config/default.py
@@ -4,3 +4,4 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 mongodb_host = "localhost"
 mongodb_port = "27017"
 mongodb_name = "aimmoPost"
+token = "abcd"
--- aimmoPost/tests/test_login.py
+++ aimmoPost/tests/test_login.py
@@ -15,7 +15,8 @@ def test_login_success(api):
     resp = api.post("/user/login", data=json.dumps(login_data), content_type="application/json")
     data = json.loads(resp.data.decode("utf-8"))
     assert resp.status_code == 200
-    assert data == {"success": True}
+    assert data["success"] == True
+    assert isinstance(data["token"], str)
 
 
 def test_login_wrongid(api):
