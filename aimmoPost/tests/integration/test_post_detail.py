75b1f4ad5d056aa57f6eefef76c89665dbf75f73
fhzpdlqk <rozencrants@naver.com>
2022-07-05 13:16:20 +0900

    post detail islike add

--- aimmoPost/tests/integration/test_post_detail.py
+++ aimmoPost/tests/integration/test_post_detail.py
@@ -17,6 +17,18 @@ def test_post_detail_success(app, db, id_token, post):
     assert isinstance(data["tag"], list)
     assert isinstance(data["notice"], bool)
     assert isinstance(data["comment"], list)
+    assert isinstance(data["is_like"], bool)
+    for i in data["comment"]:
+        assert isinstance(i["writer"], str) and len(i["writer"]) > 0
+        assert isinstance(i["date"], str) and len(i["date"]) > 0
+        assert isinstance(i["content"], str) and len(i["content"]) > 0
+        assert isinstance(i["recomment"], list)
+        assert isinstance(i["is_like"], bool)
+        for j in i["recomment"]:
+            assert isinstance(j["writer"], str) and len(j["writer"]) > 0
+            assert isinstance(j["date"], str) and len(j["date"]) > 0
+            assert isinstance(j["content"], str) and len(j["content"]) > 0
+            assert isinstance(j["is_like"], bool)
 
 
 def test_post_detail_wrongid(app, db, id_token, post):
