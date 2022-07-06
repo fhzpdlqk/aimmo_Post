41ae2c774943a638b18d9c4fc4a99bdbd0bbbbf9
fhzpdlqk <rozencrants@naver.com>
2022-07-06 11:00:00 +0900

    board regist markup add

--- aimmoPost/aimmoPost/Route/board.py
+++ aimmoPost/aimmoPost/Route/board.py
@@ -8,6 +8,21 @@ from flask_classful import FlaskView, route
 
 
 class BoardView(FlaskView):
+    """
+    게시판 생성 API
+    method: POST
+    content-type: application/json
+    uri: "/board/regist"
+    request : {
+        boardname: 게시판 이름
+    }
+    response : {
+        성공시 : {success: true}, 200
+        게시판 이름이 누락되었을 경우 : {success: false,message:"게시판 이름을 입력하세요"}, 400
+        이외의 오류가 발생했을 경우 :{success: false, message: error.message},500
+    }
+    """
+
     @route("/regist", methods=["POST"])
     def board_regist(self):
         try:
