8d60373b4f100ded51defc589bfde49a98debc0f
fhzpdlqk <rozencrants@naver.com>
2022-07-07 11:40:49 +0900

    mypage my_post add

--- aimmoPost/aimmoPost/Route/mypage.py
+++ aimmoPost/aimmoPost/Route/mypage.py
@@ -4,7 +4,7 @@ import sys
 import jwt
 import json
 from flask_classful import FlaskView, route
-from aimmoPost.aimmoPost.models.Post import Post
+from aimmoPost.aimmoPost.models.Post import Post, PostListSchema
 from aimmoPost.aimmoPost.config import default
 
 token_key = default.token
@@ -15,9 +15,12 @@ class MyPageView(FlaskView):
     def my_post(self):
         try:
             decoded = jwt.decode(request.headers["Token"], token_key, algorithms="HS256")
-            print(decoded["user_id"])
-            post = Post.objects()
-            print(post)
+            posts = Post.objects()
+            result = []
+            for post in posts:
+                result.append(PostListSchema().dump(post))
+            return jsonify({"success": True, "message": result}), 200
+        except jwt.exceptions.InvalidSignatureError:
+            return {"success": False, "message": "아이디 토큰이 잘못되었습니다"}, 401
         except:
-            print(str(sys.exc_info()))
-        return "aa"
+            return {"success": False, "message": str(sys.exc_info())}, 500
