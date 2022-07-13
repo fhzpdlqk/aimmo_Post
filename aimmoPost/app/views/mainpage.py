c30061e493aae012367396a1d1d867fd0c0d8f12
fhzpdlqk <rozencrants@naver.com>
2022-07-13 17:40:13 +0900

    mainpage apispec update

--- aimmoPost/app/views/mainpage.py
+++ aimmoPost/app/views/mainpage.py
@@ -1,20 +1,28 @@
 from flask import jsonify
 from flask_classful import FlaskView, route
+from flask_apispec import use_kwargs, marshal_with
 from app.schemas.PostSchema import PostListSchema
+from app.decorator import login_required
 from app.models import Post
 
 class MainPageView(FlaskView):
     @route("/recent", methods=["GET"])
+    @login_required
+    @marshal_with(PostListSchema(many=True), code=200, description="게시물 최근목록 10개")
     def recent_post(self):
         post_list = Post.objects().order_by("-date")[:10]
-        return jsonify(message=PostListSchema(many=True).dump(post_list)), 200
+        return post_list, 200
 
     @route("/comment", methods=["GET"])
+    @login_required
+    @marshal_with(PostListSchema(many=True), code=200, description="게시물 댓글순목록 10개")
     def comment_post(self):
         post_list = Post.objects().order_by("-num_comment")[:10]
-        return jsonify(message=PostListSchema(many=True).dump(post_list)), 200
+        return post_list, 200
 
     @route("/like", methods=["GET"])
+    @login_required
+    @marshal_with(PostListSchema(many=True), code=200, description="게시물 좋아요순목록 10개")
     def like_post(self):
         post_list = Post.objects().order_by("-like")[:10]
-        return jsonify(message=PostListSchema(many=True).dump(post_list)), 200
+        return post_list, 200
--- aimmoPost/tests/views/test_mainpage.py
+++ aimmoPost/tests/views/test_mainpage.py
@@ -1,43 +1,60 @@
 from json import dumps
 import pytest
+import random
+import jwt
+from app.config import TestConfig
+from tests.factory.post_factory import PostFactory
+from tests.factory.comment_factory import CommentFactory
+from tests.factory.user_factory import UserFactory
 
 class Test_MainPageView:
+    @pytest.fixture
+    def login_user(self):
+        return UserFactory.create()
+
+    @pytest.fixture
+    def token(self, login_user):
+        return jwt.encode({"user_id": login_user["user_id"], "is_master": login_user["is_master"]},
+                          TestConfig.TOKEN_KEY, TestConfig.ALGORITHM)
+    @pytest.fixture
+    def post(self):
+        PostFactory.create(num_comment=random.randint(1,10))
     class Test_Recent_List:
         @pytest.fixture
-        def trans_api(self, client):
-            return client.get('/mainpage/recent', content_type="application/json")
+        def trans_api(self, client, post, headers):
+            return client.get('/mainpage/recent', headers=headers)
 
         def test_상태코드_200(self,trans_api):
             assert trans_api.status_code == 200
 
         def test_목록_최신순_여부(self, trans_api):
-            post_list = trans_api.json["message"]
+            post_list = trans_api.json
             for index in range(1,len(post_list)):
                 assert post_list[index-1]["date"] >= post_list[index]["date"]
 
 
     class Test_Comment_List:
         @pytest.fixture
-        def trans_api(self, client):
-            return client.get('/mainpage/comment', content_type="application/json")
+        def trans_api(self, client, post, headers):
+            return client.get('/mainpage/comment', headers=headers)
 
         def test_상태코드_200(self,trans_api):
             assert trans_api.status_code == 200
 
-        def test_목록_최신순_여부(self, trans_api):
-            post_list = trans_api.json["message"]
+        def test_목록_댓글순_여부(self, trans_api):
+            post_list = trans_api.json
             for index in range(1,len(post_list)):
                 assert post_list[index-1]["num_comment"] >= post_list[index]["num_comment"]
 
     class Test_like_List:
         @pytest.fixture
-        def trans_api(self, client):
-            return client.get('/mainpage/like', content_type="application/json")
+        def trans_api(self, client, post, headers):
+            return client.get('/mainpage/like', headers=headers)
 
         def test_상태코드_200(self,trans_api):
             assert trans_api.status_code == 200
 
-        def test_목록_최신순_여부(self, trans_api):
-            post_list = trans_api.json["message"]
+        def test_목록_좋아요순_여부(self, trans_api):
+            post_list = trans_api.json
             for index in range(1,len(post_list)):
                 assert post_list[index-1]["num_like"] >= post_list[index]["num_like"]
